22ae05ed23330ff850840f675f31716efbae9f49
christopherlindberg1 <christopher.lindberg1@gmail.com>
2021-04-30 19:08:32 +0200

    Added appropriate labels for all fields

--- app/forms/dialogue.py
+++ app/forms/dialogue.py
@@ -3,4 +3,8 @@ from wtforms.validators import Email, DataRequired, EqualTo, NumberRange
 
 
 class Dialogue(Form):
-    pass
+    emely_random_question = StringField("Emely is asking you", validators=[DataRequired()])
+    user_answer_one = TextAreaField("How would you reply to Emely's question?", validators=[DataRequired()])
+    user_follow_up_one = TextAreaField("What would Emely's follow-up question be to your answer?", validators=[DataRequired()])
+    user_answer_two = TextAreaField("How would you answer Emely's follow-up question?", validators=[DataRequired()])
+    user_follow_up_two = TextAreaField("What would Emely's follow-up question be to this answer?", validators=[DataRequired()])
