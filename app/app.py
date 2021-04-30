2aea9d1777dc73a33f5b8a6580adaf2da41f5de1
christopherlindberg1 <christopher.lindberg1@gmail.com>
2021-04-30 22:24:52 +0200

    Set up structure for saving dialogue. Smaller methods not implemented yet

--- app/app.py
+++ app/app.py
@@ -2,32 +2,59 @@ from flask import Flask, render_template, request, flash, redirect, url_for, ses
 
 import os
 from pathlib import Path
+import sys
+sys.path.append("../")  # Needed to access config file outside of repo
 
-from data_access.text_file_reader import TextFileReader
+import config
+# from data_access.text_file_reader import TextFileReader
+from core_functionality.dialogue_saver import DialogueSaver
 from core_functionality.question_generator import QuestionGenerator
+
 from forms.dialogue import Dialogue
 
 
 app = Flask(__name__)
+app.config["SECRET_KEY"] = config.secret_key
 
-text_file_reader = TextFileReader()
+# text_file_reader = TextFileReader()
 question_generator = QuestionGenerator()
+dialogue_saver = DialogueSaver()
+
 
 @app.route("/")
 def index():
     """ Returns the landing page """
 
     form = Dialogue(request.form)
-    question = question_generator.get_random_question()
+    base_question = question_generator.get_random_question()
+    session["base_question"] = base_question
 
-    return render_template("index.html", form = form, question = question)
+    return render_template("index.html", form = form, base_question = base_question)
 
 
 @app.route("/submit-dialogue/", methods = ["POST"])
 def submit_dialogue():
     """ Saves the user generated data to a text file """
-    if request.method == "POST":
-        return "Hell no."
+    form = Dialogue(request.form)
+
+    if form.validate() == False:
+        flash("You dialogue was not submitted correctly", "danger")
+        return redirect(url_for("index"))
+    
+    base_question = session["base_question"]
+    a_one = request.form["user_answer_one"]
+    f_one = request.form["user_follow_up_one"]
+    a_two = request.form["user_answer_two"]
+    f_two = request.form["user_follow_up_two"]
+    # print(f"OOOOKKKKK...: { base_question }")
+
+    dialogue_saver.save_dialogue(base_question, a_one, f_one, a_two, f_two)
+    
+    session["base_question"] = None
+    
+    flash("Thank you", "success")
+    return redirect(url_for("index"))
+        
 
 if __name__ == "__main__":
     app.run(debug=True)
