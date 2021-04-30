6b5479b0100e53116d31637307550e4a3585acfa
christopherlindberg1 <christopher.lindberg1@gmail.com>
2021-04-30 12:05:05 +0200

    Initial commit

---
+++ app/static/js/main.js
@@ -0,0 +1,35 @@
+
+
+// Smooth scroll on nav click
+$(".nav-link, .navbar-brand").on("click", function(e) {
+    try {
+        if (this.hash !== "") {
+            e.preventDefault();
+            const hash =  this.hash;
+            $("html, body").animate({
+                scrollTop: $(hash).offset().top - 56
+            }, 0);
+        }
+    }
+    catch (ex) {
+        if (e.target.innerText == "About me") {
+            window.location = "./../#section-about";
+        } else if (e.target.innerText == "Projects") {
+            window.location = "./../#section-projects";
+        }
+    }
+});
+
+// Close menu links on link click
+$(function(){ 
+    var navMain = $(".navbar-collapse");
+    navMain.on("click", "a:not([data-toggle])", null, function () {
+        navMain.collapse('hide');
+    });
+});
+
+// Close menu when clicking outside of it
+document.getElementById("main").addEventListener("click", e => {
+    var navMain = $(".navbar-collapse");
+    navMain.collapse('hide');
+});
