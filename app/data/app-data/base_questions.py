f44afa2ed1a780bd5ee4dca658e8b4bce0ba4a94
christopherlindberg1 <christopher.lindberg1@gmail.com>
2021-04-30 17:31:58 +0200

    Renamed folder from app-data to app_data

--- app/data/app-data/base_questions.py
+++
@@ -1,6 +0,0 @@
-base_questions = [
-    "What makes you a good fit?",
-    "If you are going to work with us, it is good if you have experience from working with others. Can you tell me a little about if you have such experience?",
-    "Is there anything you want to ask about this job?",
-    "What is your best experience from your previous jobs?",
-]
