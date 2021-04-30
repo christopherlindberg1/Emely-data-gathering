797fec2c5353c5d96edf2ace76d82e98c083bf27
christopherlindberg1 <christopher.lindberg1@gmail.com>
2021-04-30 16:35:25 +0200

    Added import statement for fields and validators

--- app/forms/dialogue.py
+++ app/forms/dialogue.py
@@ -1,2 +1,6 @@
-class Dialogue():
+from wtforms import Form, StringField, TextAreaField, PasswordField, SelectField, BooleanField, IntegerField
+from wtforms.validators import Email, DataRequired, EqualTo, NumberRange
+
+
+class Dialogue(Form):
     pass
