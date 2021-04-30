bdeaf25f119ac09e9cfbc86a0e5692fce89e6f89
christopherlindberg1 <christopher.lindberg1@gmail.com>
2021-04-30 22:21:39 +0200

    Added comment for class

--- app/forms/dialogue.py
+++ app/forms/dialogue.py
@@ -3,6 +3,7 @@ from wtforms.validators import DataRequired
 
 
 class Dialogue(Form):
+    """ Class used for handling the user input in the dialogue with Emily """
     user_answer_one = TextAreaField("How would you reply to Emely's question?", validators=[DataRequired()])
     user_follow_up_one = TextAreaField("What would Emely's follow-up question be to your answer?", validators=[DataRequired()])
     user_answer_two = TextAreaField("How would you answer Emely's follow-up question?", validators=[DataRequired()])
