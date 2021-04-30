b98ecfec056a500f4211c7afd6ee198246380755
christopherlindberg1 <christopher.lindberg1@gmail.com>
2021-04-30 20:48:25 +0200

    Created class TextFileWriter with empty method write_lines

---
+++ app/data_access/text_file_writer.py
@@ -0,0 +1,12 @@
+
+
+class TextFileWriter:
+    """ Class with methods that read from text files """
+
+    def __init__(self):
+        pass
+
+    
+    def write_lines(self):
+        """ Reads all lines from a text file and returns them as a list of strings """
+        pass
