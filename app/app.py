b5f4ef85332e64fc24ba99939e4c2ec4075a376c
christopherlindberg1 <christopher.lindberg1@gmail.com>
2021-04-30 17:40:04 +0200

    Now passing a random question to the view

--- app/app.py
+++ app/app.py
@@ -1,12 +1,28 @@
 from flask import Flask, render_template, request, flash, redirect, url_for, session
 
+import os
+from pathlib import Path
+
+from data_access.text_file_reader import TextFileReader
+from core_functionality.question_generator import QuestionGenerator
+from forms.dialogue import Dialogue
+
+
+
 
 app = Flask(__name__)
+
+text_file_reader = TextFileReader()
+question_generator = QuestionGenerator()
 
 @app.route("/")
 def index():
     """ Returns the landing page """
-    return render_template("index.html")
+
+    form = Dialogue(request.form)
+    question = question_generator.get_random_question()
+
+    return render_template("index.html", form = form, question = question)
 
 
 if __name__ == "__main__":
