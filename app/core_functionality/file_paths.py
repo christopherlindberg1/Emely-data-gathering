cc819638f48bec9d3b7f1592d652685c726bdee4
christopherlindberg1 <christopher.lindberg1@gmail.com>
2021-04-30 23:18:37 +0200

    Added path to dialogues root folder

--- app/core_functionality/file_paths.py
+++ app/core_functionality/file_paths.py
@@ -1,8 +1,10 @@
-from pathlib import Path
+from pathlib import Path, PurePath
 
 
 class FilePaths:
     """ Class containing class variables for file paths that are used in the app """
 
     project_root = Path(__file__).parent.parent
+
+    dialogues_folder = PurePath(project_root, "data/generated_data")
 
