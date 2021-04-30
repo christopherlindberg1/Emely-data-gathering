1a7c4c26e8681983cedd6c7772b6576abddce248
christopherlindberg1 <christopher.lindberg1@gmail.com>
2021-04-30 22:19:30 +0200

    Created class responsible for handling the file structure for dialogue files

---
+++ app/core_functionality/dialogue_file_manager.py
@@ -0,0 +1,15 @@
+
+
+class DialogueFileManager:
+    """ Class responsible for creating an appropriate file structure for dialogue files """
+    
+    def __init__(self):
+        pass
+
+
+    def get_unique_file_path(self):
+        """ Returns a unique file name for a dialogue """
+
+        return "so/unique.txt" 
+        
+        
