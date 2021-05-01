8efbdf45fb3215e49d6be4c1af29d1b6f982fd4f
christopherlindberg1 <christopher.lindberg1@gmail.com>
2021-05-01 15:07:59 +0200

    Refactored methods to take in a datetime object passed in from a higher level method instead of using datetime.now() hardcoded in each method

--- app/core_functionality/dialogue_file_manager.py
+++ app/core_functionality/dialogue_file_manager.py
@@ -17,7 +17,9 @@ class DialogueFileManager:
     def get_unique_file_path(self):
         """ Returns a unique file name for a dialogue """
 
-        folder_path = self.__get_folder_for_today()
+        current_datetime = datetime.datetime.now()
+
+        folder_path = self.__get_folder_for_datetime(current_datetime)
         full_file_path = self.__get_unique_file_name(folder_path)
 
         return full_file_path
@@ -29,45 +31,37 @@ class DialogueFileManager:
         Makes sure it is unique before returning it.
         """
 
-        # file_name = f"{ datetime.datetime.now().strftime('%Y/%m/%d-%I:%M:%S.%f') }-{ randrange(1, 1000) }.txt"
         file_name = f"{ randrange(1, 100000001) }.txt"
         full_file_path = PurePath(folder_path, file_name)
 
         while os.path.isfile(full_file_path) == True:
-            # file_name = f"{ datetime.datetime.now().strftime('%Y/%m/%d-%I:%M:%S.%f') }-{ randrange(1, 1000) }.txt"
             file_name = f"{ randrange(1, 100000001) }.txt"
             full_file_path = PurePath(folder_path, file_name)
         
         return full_file_path
 
 
-    def __get_folder_for_today(self):
+    def __get_folder_for_datetime(self, date_time):
         """ Returns the path to the folder that match the current date """
 
-        date = datetime.datetime.now()
-        
-        year = date.year
-        month = date.month if len(str(date.month)) == 2 else f"0{ date.month }"
-        # month = 6 if len(str(6)) == 2 else f"0{ 6 }"
-        day = date.day if len(str(date.day)) == 2 else f"0{ date.day }"
+        year = date_time.year
+        month = date_time.month if len(str(date_time.month)) == 2 else f"0{ date_time.month }"
+        day = date_time.day if len(str(date_time.day)) == 2 else f"0{ date_time.day }"
 
         folder_path = PurePath(FilePaths.dialogues_folder, f"{ year }/{ month }/{ day }")
 
         if os.path.isdir(folder_path) == True:
             return folder_path
         else:
-            return self.__create_folder_for_today_and_return_path()
+            return self.__create_folder_for_datetime_and_return_path(date_time)
 
 
-    def __create_folder_for_today_and_return_path(self):
+    def __create_folder_for_datetime_and_return_path(self, date_time):
         """ Creates folder for the current day and returns the path """
 
-        date = datetime.datetime.now()
-        
-        year = date.year
-        month = date.month if len(str(date.month)) == 2 else f"0{ date.month }"
-        # month = 6 if len(str(6)) == 2 else f"0{ 6 }"
-        day = date.day if len(str(date.day)) == 2 else f"0{ date.day }"
+        year = date_time.year
+        month = date_time.month if len(str(date_time.month)) == 2 else f"0{ date_time.month }"
+        day = date_time.day if len(str(date_time.day)) == 2 else f"0{ date_time.day }"
 
         year_folder = PurePath(FilePaths.dialogues_folder, f"{ year }")
 
