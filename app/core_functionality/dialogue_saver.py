c1531fa4bb7a14e213a45ea091f82547bf13b3a2
christopherlindberg1 <christopher.lindberg1@gmail.com>
2021-05-01 21:54:24 +0200

    Cleaned up file, added type for arguments

--- app/core_functionality/dialogue_saver.py
+++ app/core_functionality/dialogue_saver.py
@@ -1,6 +1,7 @@
 from core_functionality.dialogue_file_manager import DialogueFileManager
 from data_access.text_file_writer import TextFileWriter
 
+
 class DialogueSaver:
     """ Class that handles the opreations of writing user generated dialogues to the file system """
     
@@ -9,16 +10,22 @@ class DialogueSaver:
         self.text_file_writer = TextFileWriter()
 
 
-    def save_dialogue(self, base_question, a_one, f_one, a_two, f_two):
+    def save_dialogue(
+        self,
+        base_question: str,
+        answer_one: str,
+        follow_up_one: str,
+        answer_two: str,
+        follow_up_two: str):
         """ Formats dialogue data and saves it in a file """
         
         dialogue_lines = [
             "episode_start\n",
             f"emely: { base_question }\n",
-            f"user: { a_one }\n",
-            f"emely: { f_one }\n",
-            f"user: { a_two }\n",
-            f"emely: { f_two }\n",
+            f"user: { answer_one }\n",
+            f"emely: { follow_up_one }\n",
+            f"user: { answer_two }\n",
+            f"emely: { follow_up_two }\n",
             "episode_done\n"
         ]
 
