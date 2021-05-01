0d4c856b9505d7c4e31f49c79539ff207f2cda2b
christopherlindberg1 <christopher.lindberg1@gmail.com>
2021-05-01 22:15:35 +0200

    Added paths app_data_folder and base_questions_text_file

--- app/core_functionality/file_paths.py
+++ app/core_functionality/file_paths.py
@@ -6,5 +6,9 @@ class FilePaths:
 
     project_root = Path(__file__).parent.parent
 
+    app_data_folder = PurePath(project_root, "data/app_data")
+
+    base_questions_text_file = PurePath(app_data_folder, "base_questions.txt")
+
     dialogues_folder = PurePath(project_root, "data/generated_data/dialogues")
 
